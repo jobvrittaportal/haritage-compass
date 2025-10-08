2e7f9c83b3ec7b2a71aeff589142a8c18afee645
Shivam12Aryan <aryan.singh@tekinspirations.com>
2025-10-08 21:30:54 +0530

    blog refactor

--- TourTravel/ViewComponents/BlogViewComponent.cs
+++ TourTravel/ViewComponents/BlogViewComponent.cs
@@ -1,32 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using System.Linq;
+using TourTravel.Models;
+using TourTravel.ViewModel;
 
 namespace TourTravel.ViewComponents
 {
-    //public class BlogViewComponent(MyDbContext db) : ViewComponent
-    //{
-    //    public IViewComponentResult Invoke(bool showHeading = true)
-    //    {
-    //        var blogs = db.Blogs
-    //                            .OrderByDescending(b => b.CreatedAt)
-    //                            .Take(3)
-    //                            .ToList();
-
-    //        ViewData["ShowHeading"] = showHeading;
-
-    //        return View(blogs);
-    //    }
-    //}
-
-    public class BlogViewComponent : ViewComponent
+   
+    public class BlogViewComponent(MyDbContext db) : ViewComponent
     {
-        public IViewComponentResult Invoke(bool showHeading )
+        public IViewComponentResult Invoke(bool  showheading, int take)
         {
-
-            ViewData["ShowHeading"] = showHeading;
-
-            return View();
+            var blog = db.Blog.OrderByDescending(b => b.Id).Take(take).ToList();
+            var viewModel = new BlogViewModel
+            {
+                Blogs = blog,
+                ShowHeading = showheading
+            };
+            return View(viewModel);
         }
     }
 }
