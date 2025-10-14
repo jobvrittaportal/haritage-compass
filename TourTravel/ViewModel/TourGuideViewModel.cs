b06855f059f0bf8c2e84c483e616b7799aa0ed17
Shivam12Aryan <aryan.singh@tekinspirations.com>
2025-10-14 16:38:19 +0530

    TOURGUIDE AND OTHER

---
+++ TourTravel/ViewModel/TourGuideViewModel.cs
@@ -0,0 +1,16 @@
+﻿using TourTravel.Models;
+
+namespace TourTravel.ViewModel
+{
+    public class TourGuideViewModel
+    {
+        public IEnumerable<TourTravel.Models.TourGuideView> TourGuides { get; set; } = new List<TourTravel.Models.TourGuideView>();
+        public bool ShowHeading { get; set; } = true;
+    }
+    public class TourGuideDetail
+    {
+       public  required TourGuideView TourGuide { get; set; }
+
+    }
+
+}
